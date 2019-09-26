ac7d36f7d82ffc860f19b60f7300a034588bee64
B-Schulz <bjoern.schulz1991@googlemail.com>
2019-09-26 20:01:08 +0200

    evaluate tuple in config

--- raspi/GpioController.py
+++ raspi/GpioController.py
@@ -17,8 +17,7 @@ class GpioController:
         # self.pins = pin_numbers
         self.pins = dict([(v, k) for k, v in config.items('GPIOS')])
         # self.measurement_names = dict(config.items('MEASUREMENT_NAMES'))
-        self.measurement_mapping = dict(config.items('MEASUREMENT_MAPPINGS'))
-        print(self.measurement_mapping)
+        self.measurement_mapping = {k: eval(v) for k, v in config.items('MEASUREMENT_MAPPINGS')}
 
         self.power_measurement_mapping = {
             input: {
